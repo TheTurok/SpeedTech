b8677b31829393f742e450fa135e262af21944c5
Timothy Choi <TurokChoi@gmail.com>
2018-03-31 18:15:38 -0700

    server Side

---
+++ Assets/Scripts/Server.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using UnityEngine;
+
+public class Server : MonoBehaviour
+{
+
+    public int port = 6321;
+
+    private List<ServerClient> clients;
+    private List<ServerClient> disconnectedList;
+
+    private TcpListener server;
+    private bool serverStarted;
+
+    public void Init()
+    {
+        DontDestroyOnLoad(gameObject);
+        clients = new List<ServerClient>();
+        disconnectedList = new List<ServerClient>();
+
+        try
+        {
+            server = new TcpListener(IPAddress.Any, port);
+            server.Start();
+
+            StartListening();
+            serverStarted = true;
+
+        }
+        catch (Exception e)
+        {
+            Debug.Log("Socket error: " + e.Message);
+        }
+    }
+
+    private void Update()
+    {
+        if (!serverStarted)
+            return;
+
+        foreach(ServerClient c in clients)
+        {
+            //if client still connected?
+            if (!IsConnected(c.tcp))
+            {
+                c.tcp.Close();
+                disconnectedList.Add(c);
+                continue;
+            }
+            else
+            {
+                NetworkStream s = c.tcp.GetStream();
+                if (s.DataAvailable)
+                {
+                    StreamReader reader = new StreamReader(s,true);
+                    string data = reader.ReadLine();
+
+                    if(data != null)
+                    {
+                        OnIncomingData(c, data);
+                    }
+                }
+            }
+
+        }
+
+        for(int i = 0; i < disconnectedList.Count - 1; i++)
+        { //tell players somebody has disconnected
+            clients.Remove(disconnectedList[i]);
+            disconnectedList.RemoveAt(i);
+        }
+    }
+
+    private void StartListening()
+    {
+        server.BeginAcceptTcpClient(AcceptTcpClient, server);
+    }
+
+    private void AcceptTcpClient(IAsyncResult ar)
+    {
+        TcpListener listener = (TcpListener)ar.AsyncState;
+
+        ServerClient sc = new ServerClient(listener.EndAcceptTcpClient(ar));
+        clients.Add(sc);
+
+        StartListening();
+
+        Debug.Log("somebody has connected");
+    }
+
+    private bool IsConnected(TcpClient c)
+    {
+        try
+        {
+            if(c != null && c.Client != null && c.Client.Connected)
+            {
+                if (c.Client.Poll(0, SelectMode.SelectRead))
+                    return !(c.Client.Receive(new byte[1], SocketFlags.Peek) == 0;
+
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        catch
+        {
+            return false;
+        }
+    }
+
+    private void OnIncomingData(ServerClient c, string data){
+        Debug.Log(c.clientName + " : " + data);
+    }
+}
+
+public class ServerClient
+{
+    public string clientName;
+    public TcpClient tcp;
+
+    public ServerClient(TcpClient tcp)
+    {
+        this.tcp = tcp;
+    }
+}
